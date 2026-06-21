18f06ab96685d33ba1c67b30903752c3b0f36e73
Konstantin Vashkevich <89992059760@bk.ru>
2026-06-21 21:32:17 +0300

    Claude redesign v9: full design system, tokens, assets, uploads

---
+++ _ds_bundle.js
@@ -0,0 +1,11 @@
+/* @ds-bundle: {"format":3,"namespace":"TradeRateDesignSystem_aace51","components":[],"sourceHashes":{},"inlinedExternals":[],"unexposedExports":[]} */
+
+(() => {
+
+const __ds_ns = (window.TradeRateDesignSystem_aace51 = window.TradeRateDesignSystem_aace51 || {});
+
+const __ds_scope = {};
+
+(__ds_ns.__errors = __ds_ns.__errors || []);
+
+})();
